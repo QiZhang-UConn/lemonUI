9378548ed96f77c101b2df6e0c188a9c65de6b37
Danhtran123 <47044112+Danhtran123@users.noreply.github.com>
2021-12-22 10:53:32 -0500

    refactored youtube API stuff to their own util class

--- components/dashboard/dashboard.js
+++ components/dashboard/dashboard.js
@@ -1,6 +1,7 @@
 import { ViewComponent } from "../view.js";
 import { Router } from "../../util/router.js";
 import router from '../../app.js';
+import getYTData from "../../util/youtube.js";
 
 
 // TODO: 
@@ -58,9 +59,6 @@ function DashboardComponent() {
 
     // Button for logout
     let logoutLink;
-
-    // Key for youtube API requests
-    let youtubeKey = 'AIzaSyBOtxA3v2ZiF7uZc854aNvtjznJ-qBezU0';
 
     let publicListBtn;
     let deleteBtn;
@@ -229,50 +227,6 @@ function DashboardComponent() {
 
         return dfd.promise();
     }
-
-
-    //_____________Song Functionality_______________
-    async function getYTData(url){
-        // Construct an empty details item
-        let songDetails;
-        let id;
-        // First do a YT search in order to get the video's title
-        // Construct our request
-        const requestUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${url}&key=${youtubeKey}`;
-        // Create a fetch
-        await fetch(requestUrl)
-        .then(response => response.json())
-        .then(async data => { 
-            songDetails = {"title":data.items[0].snippet.title, "duration":undefined};
-            console.log("The title:" + songDetails.title);
-            id = data.items[0].id.videoId;
-        })
-
-        songDetails = {"title":songDetails.title, "duration":await getDuration(id)};
-        console.log(songDetails);
-        return songDetails;
-
-
-    }
-
-     async function getDuration(titleId){
-        //let respo = await fetch()
-        // Take the data from the previous search and use it to get the duration
-        let duration;
-        // Construct our query
-        const requestUrl = `https://www.googleapis.com/youtube/v3/videos?part=contentDetails&maxResults=1&id=${titleId}&key=${youtubeKey}`;
-
-        // Perform a fetch
-        await fetch(requestUrl)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.items[0].contentDetails.duration);
-            duration = data.items[0].contentDetails.duration;
-        })
-
-        return duration;
-    }
-
 
     // Adds a song to the playlist
     async function addSongs(){
